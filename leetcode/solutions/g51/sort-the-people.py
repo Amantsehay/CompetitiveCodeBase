1ba42aa1b7bae750fa26b0f541d2fe9b3b93922d
Amantsehay <130090452+Amantsehay@users.noreply.github.com>
2023-12-26 19:10:17 +0300

    Added solution for Sort the People

--- leetcode/solutions/g51/sort-the-people.py
+++ leetcode/solutions/g51/sort-the-people.py
@@ -1,9 +1,16 @@
 class Solution:
     def sortPeople(self, names: List[str], heights: List[int]) -> List[str]:
         for i in range(len(names)):
-            for j in range(len(names) - i - 1):
-                if heights[j] < heights[j + 1]:
-                    heights[j], heights[j + 1] = heights[j+ 1], heights[j]
-                    names[j], names[j + 1] = names[j+ 1], names[j]
+            maxx = heights[i]
+            k = i
+            for j in range(i + 1, len(names)):
+                if heights[j] > maxx:
+                    maxx = heights[j]
+                    k = j
+            names[i], names[k] = names[k], names[i]
+            heights[i], heights[k] = heights[k], heights[i]
         return names
+                
+
+        
         
