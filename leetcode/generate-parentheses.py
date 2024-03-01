a3eec70e2e344ec671ef6373d169aff0a8deb2df
Amantsehay <130090452+Amantsehay@users.noreply.github.com>
2024-03-01 07:31:01 +0000

    Add solution for Generate Parentheses

--- leetcode/generate-parentheses.py
+++ leetcode/generate-parentheses.py
@@ -1,8 +1,26 @@
-class Solution(object):
-    def generateParenthesis(self, n):
-        dp = [[] for i in range(n + 1)]
-        dp[0].append('')
-        for i in range(n + 1):
-            for j in range(i):
-                dp[i] += ['(' + x + ')' + y for x in dp[j] for y in dp[i - j - 1]]
-        return dp[n]
+class Solution:
+    def generateParenthesis(self, n: int) -> List[str]:
+        
+        result=[]
+        def back_track(oc,cc,curr):
+            
+            if oc==cc==n:
+                result.append(''.join(curr))
+                return 
+                
+            if oc<n:                
+
+                curr.append('(')
+                back_track(oc+1,cc,curr)
+                curr.pop()
+                
+            if cc<oc:
+                curr.append(')')
+                back_track(oc,cc+1,curr)
+                curr.pop()
+                
+                
+        back_track(0,0,[])   
+        
+        return result                                                                                                                                                                                                                                                                   
+       
